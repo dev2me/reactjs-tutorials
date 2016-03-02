e1fc95ceadf867d5ade1cdcb0cf8490a278f91a6
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-03-01 21:29:26 -0600

    se optimizan los componentes y se añaden más estados para la aplicación

--- CursoReact/eventos/main.js
+++ CursoReact/eventos/main.js
@@ -2,7 +2,8 @@
 var Comida = React.createClass({
 	getInitialState : function() {
 		return {
-			like: Boolean(this.props.like)
+			like: Boolean(this.props.like),
+			editing: false
 		}
 	},
 	handleLike: function(){
@@ -10,9 +11,39 @@ var Comida = React.createClass({
 			like: !this.state.like
 		})
 	},
-	render: function() {
+
+	edit: function() {
+		this.setState({editing: true});
+	},
+
+	save: function() {
+		this.props.onChange(this.refs.nuevoNombre.value, this.props.index);	
+		this.setState({editing: false});
+	},
+
+	remove: function() {
+		this.props.onRemove(this.props.index);
+	},
+
+	cancel: function() {
+		this.setState({editing: false});
+	},
+
+	showEditingView: function() {
 		return (
-			<div>
+			<div className="comida">
+				<input ref="nuevoNombre" type="text" className="form-control" placeholder="Nuevo nombre" defaultValue={this.props.nombre}/>
+				<div>
+					<div className="glyphicon glyphicon-ok-circle blue" onClick={this.save} />
+					<div className="glyphicon glyphicon-remove-circle red" onClick={this.cancel} />
+				</div>
+			</div>
+		)
+	},
+
+	showFinalView : function() {
+		return (
+			<div className="comida">
 				<h1 className="bg-success">{this.props.nombre}</h1>
 				<p className="bg-info">
 					Comida <i>{this.props.children}</i>
@@ -25,23 +56,109 @@ var Comida = React.createClass({
 					<br/>
 					Like: {String(this.state.like)}
 				</p>
+				<div>
+				<div className="glyphicon glyphicon-pencil blue" onClick={this.edit} />
+					<div className="glyphicon glyphicon-trash red" onClick={this.remove} />
+				</div>
 			</div>
 		);
+	},
+
+	render: function() {
+		if(this.state.editing) {
+			return this.showEditingView();
+		} else {
+			return this.showFinalView();
+		}
 	}
 });
 
-ReactDOM.render(
-	<div className="centerBlock">
-		<Comida nombre="Tacos" like>
-			Mexicana
-		</Comida>
-		<Comida nombre="Paella">
-			Española
-		</Comida>
-		<Comida nombre="Ceviche" like>
-			Peruana
-		</Comida>
-	</div>,
 
-	document.getElementById('container')
-);
+/*Modulo Lista Comida*/
+
+var ListaComida = React.createClass({
+
+	getInitialState: function() {
+		return {
+			comidas : [
+
+				'tacos',
+				'paella',
+				'ceviche',
+				'Mole'
+			]
+		}
+	},
+	componentWillMount: function() {
+			var pais;
+			var s = this;
+
+			$.getJSON("https://restcountries.eu/rest/v1/all", function(data) {
+				for(pais in data){
+					console.log(pais, data[pais].name);
+					s.add(data[pais].name);
+				}
+			});
+		},
+	add: function(comida){
+		var nuevaComida = this.refs.nuevaComida.value;
+
+		if(nuevaComida == "") {
+			if(typeof nuevaComida == 'undefined')
+			{
+				nuevaComida = "Nueva Comida"
+			}
+			else{
+				var arr = this.state.comidas;
+				arr.push(nuevaComida)
+				this.setState({comidas: arr});
+				this.refs.nuevaComida.value="";
+			}
+		}
+	},
+
+	update: function(nuevoNombre, i) {
+		var arr = this.state.comidas;
+		arr[i] = nuevoNombre;
+		this.setState({comidas: arr});
+	},
+
+	remove: function(i) {
+		var arr = this.state.comidas;
+		arr.splice(i, 3);
+		this.setState({comidas: arr})
+	},
+
+	eachItem: function(comida, i) {
+		return( <Comida key={i} nombre={comida} index={i} onChange={this.update}> {i+1} </Comida> )
+	},
+
+	handleKeyDown: function(e) {
+		if(e.charCode === 13) {
+			this.add();
+		}
+	},
+
+	render: function() {
+		return (
+			<div className="centerBlock">
+				<header>
+					<h1>Mis comidas favoritas</h1>
+					<i>Total: {this.state.comidas.length}</i>
+				</header>
+				<div className="input-group">
+					<input ref="nuevaComida" type="text" className="form-control" onKeyPress={this.handleKeyDown} placeholder="Agregar nueva comida..."/>
+					<span className="input-group-btn">
+						<div className="btn btn-default btn-success" onClick={this.add.bind(null, "Nueva Comida")}>+</div>
+					</span>
+				</div>
+				<div className="items">
+				{ this.state.comidas.map(this.eachItem) }
+				</div>
+			</div>
+		)
+	}
+
+});
+
+ReactDOM.render(<ListaComida />,document.getElementById('container'));
